c4b61bf0f97e05bd11198601e8274335fb1c5c11
Jim Cloudman <jim@jimcloudman.com>
2020-12-05 14:05:32 -0600

    Adding execution tests

---
+++ FalsePromise.Net/FalsePromise.Router.Tests/ExecutionTests.cs
@@ -0,0 +1,28 @@
+﻿using FalsePromise.Router.Models;
+using FalsePromise.Router.Tests.TestServices;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FalsePromise.Router.Tests
+{
+    public class ExecutionTests
+    {
+
+        [Test]
+        public void ExecuteThrowsExceptionWithBadRequest()
+        {
+            var router = new RequestRouter();
+            var service = new TestService();
+            router.Register(service);
+
+            Action act = () =>
+            {
+                router.Execute("pbbbbbbbbbbbt");
+            };
+
+            Assert.Throws<RouterException>(() => act());
+        }
+    }
+}
