3b06b4b88d9de791c605fe4d97e1ec53bb04da7f
Jim Cloudman <jim@jimcloudman.com>
2020-12-05 12:41:44 -0600

    Renamed to FalsePromise.WinUI

--- FalsePromise.Net/FalsePromise.WinUI/MessageHandler.cs
+++ FalsePromise.Net/FalsePromise.WinUI/MessageHandler.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace FalsePromise
+namespace FalsePromise.WinUI
 {
     public class MessageHandler
     {
